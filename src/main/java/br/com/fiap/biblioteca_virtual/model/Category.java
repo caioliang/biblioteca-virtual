b9555af8d6172c5be35627d2c9ee8bba0840fb98
Caio Liang <caioliang2@gmail.com>
2025-04-08 00:07:33 -0300

    add - adicionando CORS

--- src/main/java/br/com/fiap/biblioteca_virtual/model/Category.java
+++ src/main/java/br/com/fiap/biblioteca_virtual/model/Category.java
@@ -4,12 +4,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Category {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "O título não pode ser vazio")
+    @Size(min = 3, max = 50, message = "O título deve ter entre 3 e 50 caracteres")
     private String titulo; 
+    
+    @NotBlank(message = "O autor não pode ser vazio")
+    @Size(min = 3, max = 50, message = "O autor deve ter entre 3 e 50 caracteres")
     private String autor;
 
     public Long getId() {
