127f80205c1ef7c1f8541a734bf5dd0221337046
Caio Liang <caioliang2@gmail.com>
2025-04-27 21:34:22 -0300

    add - Specification e Pagination

--- biblioteca-virtual-api/src/main/java/br/com/fiap/biblioteca_virtual_api/controller/ReviewController.java
+++ biblioteca-virtual-api/src/main/java/br/com/fiap/biblioteca_virtual_api/controller/ReviewController.java
@@ -7,45 +7,52 @@ import java.math.BigDecimal;
  import org.springframework.beans.factory.annotation.Autowired;
  import org.springframework.data.domain.Example;
  import org.springframework.data.domain.ExampleMatcher;
- import org.springframework.data.domain.ExampleMatcher.StringMatcher;
- import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.ExampleMatcher.StringMatcher;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
  import org.springframework.web.bind.annotation.RequestMapping;
  import org.springframework.web.bind.annotation.RequestParam;
  import org.springframework.web.bind.annotation.RestController;
  
  import br.com.fiap.biblioteca_virtual_api.model.Review;
- import br.com.fiap.biblioteca_virtual_api.repository.ReviewRepository;
- import lombok.extern.slf4j.Slf4j;
+import br.com.fiap.biblioteca_virtual_api.model.ReviewFilter;
+import br.com.fiap.biblioteca_virtual_api.repository.ReviewRepository;
+import br.com.fiap.biblioteca_virtual_api.specification.ReviewSpecification;
+import lombok.extern.slf4j.Slf4j;
  
  @RestController
  @RequestMapping("/reviews")
  @Slf4j
  public class ReviewController {
  
-     record ReviewFilter(String description, LocalDate date, BigDecimal note){}
- 
      @Autowired
      private ReviewRepository repository;
  
      @GetMapping
 
-     public List<Review> index(ReviewFilter filter){
-         log.info("Buscando reviews com descrição {} e data {}", filter.description, filter.date);
-         var probe = Review.builder()
-                         .description(filter.description)
-                         .date(filter.date())
-                         .note(filter.note())
-                         .build();
+    //  public List<Review> index(ReviewFilter filter){
+    //      log.info("Buscando reviews com descrição {} e data {}", filter.description, filter.date);
+    //      var probe = Review.builder()
+    //                      .description(filter.description)
+    //                      .date(filter.date())
+    //                      .note(filter.note())
+    //                      .build();
  
-         var matcher = ExampleMatcher.matchingAll()
-                         .withIgnoreCase()
-                         .withIgnoreNullValues()
-                         .withStringMatcher(StringMatcher.CONTAINING);
+    //      var matcher = ExampleMatcher.matchingAll()
+    //                      .withIgnoreCase()
+    //                      .withIgnoreNullValues()
+    //                      .withStringMatcher(StringMatcher.CONTAINING);
  
-        var example = Example.of(probe, matcher);
+    //     var example = Example.of(probe, matcher);
  
-        return repository.findAll(example);
-
-     }
-     
- }
+    //     return repository.findAll(example);
+    public Page<Review> index(ReviewFilter filter, 
+        @PageableDefault(size = 10, sort = "date", direction =  Direction.DESC) 
+        Pageable pageable) {
+            return repository.findAll(ReviewSpecification.withFilters(filter), pageable);
+        }
+ 
+    }
