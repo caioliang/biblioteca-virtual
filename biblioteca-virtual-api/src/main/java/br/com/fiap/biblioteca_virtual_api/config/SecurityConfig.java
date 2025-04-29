d798d45dc3b85d7615a5dd576119b5a5182cb210
Caio Liang <caioliang2@gmail.com>
2025-04-29 17:42:42 -0300

    add - implantando Password Encoder, Roles e niveis de acesso

--- biblioteca-virtual-api/src/main/java/br/com/fiap/biblioteca_virtual_api/config/SecurityConfig.java
+++ biblioteca-virtual-api/src/main/java/br/com/fiap/biblioteca_virtual_api/config/SecurityConfig.java
@@ -4,20 +4,51 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/reviews/**").hasRole("ADMIN")
+            // .anyRequest().hasAnyRole("ADMIN", "USER")
+            .anyRequest().authenticated()
+        ) 
+        .httpBasic(Customizer.withDefaults())
+        .build();
+    }
+    
     @Bean
     UserDetailsService userDatailsServoce() {
 
         return new InMemoryUserDetailsManager(List.of(
-            User.withUsername("caio").password("{noop}12345").build()        
-            
-        ));
+            User
+                .withUsername("caio")
+                .password("$2a$12$KDEQuqJe525PqBvWoH/mBuHQqseBxPpSr1ARuyTqvZUfIofgBkRcm")
+                .roles("ADMIN")
+                .build(),        
 
+            User
+                .withUsername("celina")
+                .password("$2a$12$VwLzJ6SfrlZwvuzKfAwqRecbbsUYLkWVVg44QOEL9lRdIq9dvbApq")
+                .roles("USER")
+                .build()        
+
+            )
+        );
     }
 
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
